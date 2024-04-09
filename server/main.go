cb434b51427291c5fa707a911164aea6903e884f
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-09 16:01:37 -0700

    Create server and count html template

--- server/main.go
+++ server/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+type Templates struct {
+	templates *template.Template
+}
+
+type Count struct {
+	Count int
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("../views/*.html")),
+	}
+}
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Renderer = newTemplate()
+
+	count := Count{Count: 69}
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.Render(http.StatusOK, "index.html", count)
 	})
+
 	e.Logger.Fatal(e.Start(":3000"))
 }
