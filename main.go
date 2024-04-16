fe0e64e7a36d7b3e05c5b3aae2286673a71569db
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-15 18:22:04 -0700

    Add initial GET requests grabbing pokemon data

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,6 +19,20 @@ type Templates struct {
 
 type Count struct {
 	Count int
+}
+
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+type Pokemon struct {
+	EntryNumber int            `json:"entry_number"`
+	Species     PokemonSpecies `json:"pokemon_species"`
+}
+
+type PokemonSpecies struct {
+	Name string `json:"name"`
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
@@ -35,6 +53,38 @@ func main() {
 	count := Count{Count: 69}
 
 	e.GET("/", func(c echo.Context) error {
+		res, err := http.Get("https://pokeapi.co/api/v2/pokemon?limit=20")
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		resData, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(resData))
+		var resObject Response
+		json.Unmarshal(resData, &resObject)
+
+		// fmt.Println(resObject.Name)
+		// fmt.Println(resObject.Pokemon)
+		return c.Render(http.StatusOK, "index.html", count)
+	})
+
+	e.GET("/pokemon/:id", func(c echo.Context) error {
+		pokemonNumber := c.Param("id")
+		url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/kanto/%s", pokemonNumber)
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		resData, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf(string(resData))
 		return c.Render(http.StatusOK, "index.html", count)
 	})
 
